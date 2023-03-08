8c1260d627fb4295e45459d411619f752c43c664
rekky1aws <killikomfr@gmail.com>
2023-03-08 23:45:01 +0100

    Added main clickable zone
    Added the main clickable zone.
    A function to generate a random hexadecimal color.
    Mainclick function to change the color of the mainzone.
    Handling spacebar.

---
+++ script/app.js
@@ -0,0 +1,45 @@
+// Elements HTML
+const mainZone = document.querySelector('.mainZone');
+
+// Constantes
+
+// Variables
+
+// EventListeners
+mainZone.addEventListener('click', mainClick);
+document.body.addEventListener('keypress', keyPressed);
+
+// Functions
+function keyPressed (e)
+{
+    // console.log(e);
+    
+    switch(e.code) {
+        case 'Space':
+            mainClick();
+            break;
+    }
+}
+
+function generatecolor ()
+{
+    let rslt = "";
+    for (let index = 0; index < 6; index++) {
+        let nb = Math.floor(Math.random()*16);
+        if (nb < 10) {
+            nb += 48;
+        } else {
+            nb += 55;
+        }
+        rslt = rslt + String.fromCharCode(nb);
+    }
+    return "#" + rslt;
+}
+
+function mainClick ()
+{
+    mainZone.style.backgroundColor = generatecolor();
+}
+
+// Main code
+mainZone.style.backgroundColor = generatecolor();
